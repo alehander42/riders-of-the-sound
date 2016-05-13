c07f1731d88e2c883377e0f6ed8abbc32bd6e03f
Alexander Ivanov <alehander42@gmail.com>
2016-05-13 10:26:46 +0300

    Quick code mockup

--- game.js
+++ game.js
@@ -1,11 +1,45 @@
-var Race = (function () {
-    function Race(map) {
-        this.map = map;
-    }
-    return Race;
-}());
-function hello(sexy) {
-    return sexy;
+function game(map) {
+    var j = 0;
+    setInterval(function () {
+        if (j > map.length - 1) {
+            pronounce_command('YES WIN!');
+            pronounce_command('WELL DONE');
+        }
+        var command = action_for(map[j]);
+        if (command) {
+            pronounce_command(command);
+        }
+        // on_key(LEFT, (evt) => if(currentCommand == 'LEFT') { error -= 1; } else { error += 0.9; pronounce_command('NO! LEFT!')})
+    }, 2000);
 }
-var sexy = 'sexy';
-document.body.innerHTML = hello(sexy);
+;
+function pronounce_command(command) {
+    var synth = speechSynthesis;
+    var voices = synth.getVoices();
+    var z = new SpeechSynthesisUtterance(command);
+    for (var _i = 0, voices_1 = voices; _i < voices_1.length; _i++) {
+        var v = voices_1[_i];
+        if (v.name == 'english') {
+            z.voice = v;
+            break;
+        }
+    }
+    synth.speak(z);
+}
+function action_for(direction) {
+    if (direction < -1) {
+        return 'LEFT!';
+    }
+    else if (direction < 0) {
+        return 'left please';
+    }
+    else if (direction > 0 && direction < 1) {
+        return 'riight';
+    }
+    else if (direction > 1) {
+        return 'RIGHT!';
+    }
+    else {
+        return 'go';
+    }
+}
--- game.ts
+++ game.ts
@@ -1,12 +1,50 @@
-class Race {
 
-	constructor(private map : number[]) { }
+declare var speechSynthesis;
+declare var SpeechSynthesisUtterance;
+
+function game(map: number[]) {
+    var j = 0;
+    setInterval(() => {
+        if (j > map.length - 1) {
+            pronounce_command('YES WIN!');
+            pronounce_command('WELL DONE');
+        }
+        let command = action_for(map[j]);
+        if (command) {
+            pronounce_command(command);
+        }
+        // on_key(LEFT, (evt) => if(currentCommand == 'LEFT') { error -= 1; } else { error += 0.9; pronounce_command('NO! LEFT!')})
+    }, 2000);
+};
+
+function pronounce_command(command: string) {
+    var synth = speechSynthesis;
+    var voices = synth.getVoices();
+    var z = new SpeechSynthesisUtterance(command);
+    for (var v of voices) {
+        if (v.name == 'english') {
+            z.voice = v;
+            break;
+        }
+    }
+    synth.speak(z);    
 }
 
-function hello(sexy: string) {
-	return sexy;
+function action_for(direction: number): string {
+    if (direction < -1) {
+        return 'LEFT!';
+    } 
+    else if (direction < 0) {
+        return 'left please';
+    }
+    else if (direction > 0 && direction < 1) {
+        return 'riight';
+    }
+    else if (direction > 1) {
+        return 'RIGHT!';
+    }
+    else {
+        return 'go';
+    }
 }
-
-var sexy = 'sexy';
-
-document.body.innerHTML = hello(sexy);
+             
